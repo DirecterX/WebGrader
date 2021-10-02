2faa2d3b3055cb0caa2f5cda9ed9b5b33d870c58
DirecterX <Pusid.Suakong@gmail.com>
2021-10-02 21:25:34 +0700

    First Commit
    Create login page

---
+++ Login/Login.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="th">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+    <title>Login Grader</title>  
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/signin.css" rel="stylesheet">
+
+  </head>
+  <body class="text-center">
+    
+    <main class="form-signin">
+        <form>
+        <!-- Image Logo -->
+        <img class="mb-3" src="../Pic/Login-Logo.png" alt="" width="513" height="138" style="margin-left:-60px;">
+    
+
+        <!-- Login Form Begin -->
+        <div class="form-floating">
+        <input type="text" class="form-control" id="floatingInput" placeholder="Username">
+        <label for="floatingInput">Username</label>
+        </div>
+        <br>
+        <div class="form-floating">
+        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <label for="floatingPassword">Password</label>
+        </div>
+        <!-- Login Form End -->
+
+        <!-- Register Text -->
+        <div class="h6 mb-3 fw-normal" style="text-align: right;"><a href="">สมัครสมาชิก</a></div>
+
+        <!-- Login Buttom -->
+        <button class="w-100 btn btn-lg btn-primary lgn-btn" type="submit"><b>เข้าสู่ระบบ</b></button>
+        
+        </form>
+    </main>
+  
+  </body>
+</html>
