40e8484efc3f70a0a1ebe88daf39d843cece1bb0
DirecterX <Pusid.Suakong@gmail.com>
2021-10-05 20:32:10 +0700

    Update Home.php
    Adding bottom border of card

--- Home.php
+++ Home.php
@@ -12,9 +12,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <style>
       .cardlink{
           color:black;
+  
       }
       .cardlink:hover{
           color:#FF8540;
+      }
+      .cardborder{
+        border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;border-bottom-width: 20px;border-bottom-color: #FEC352 ;
       }
   </style>    
 
@@ -53,7 +57,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="row">
         <!-- **********************************\*Use this for generate with PHP******************************************-->
           <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card" style="background-color:#FFFFFF;">
+            <div class="card cardborder" style="background-color:#FFFFFF;border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;">
                 <a href="blankpage.php" class ="cardlink"> <!-- Link Here -->
                 <div class="card-body" >
                 
@@ -84,7 +88,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </div>
           <!-- /.col-sm-6 -->
           <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
+            <div class="card" style="border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;">
                 <div class="card-body">
                     <h5 class="card-title"><b><?php echo "Python Class" ?></b></h5> <!-- Class Name -->
 
@@ -110,7 +114,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </div>
           <!-- /.col-sm-6 -->
           <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
+            <div class="card" style="border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;">
                 <div class="card-body">
                     <h5 class="card-title"><b><?php echo "Python Class" ?></b></h5> <!-- Class Name -->
 
@@ -136,7 +140,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </div>
           <!-- /.col-sm-6 -->
           <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
+            <div class="card" style="border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;">
                 <div class="card-body">
                     <h5 class="card-title"><b><?php echo "Java Class" ?></b></h5> <!-- Class Name -->
                     
@@ -162,7 +166,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </div>
           <!-- /.col-sm-6 -->
           <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
+            <div class="card" style="border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;">
                 <div class="card-body">
                     <h5 class="card-title"><b><?php echo "Java Class" ?></b></h5> <!-- Class name -->
 
@@ -206,7 +210,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="row">
 
             <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
+            <div class="card " style=" border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;border-bottom-width: 20px;border-bottom-color: #FF8540;">
                 <a href="blankpage.php" class ="cardlink"> <!-- Link Here -->
                 <div class="card-body">
                 
@@ -243,7 +247,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div>
             <!-- /.col-sm-6 -->
             <div class="col-sm-6 col-md-4 col-lg-3">
-            <div class="card">
+            <div class="card" style="border-top-left-radius: 15px;border-top-right-radius: 15px;border-bottom-left-radius: 15px;border-bottom-right-radius: 15px;border-bottom-width: 20px;border-bottom-color: #FF8540;">
                 <div class="card-body">
                     <p class="card-text">
                             <div class="row">
