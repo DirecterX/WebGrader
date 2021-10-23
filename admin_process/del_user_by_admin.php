1a55f2ce1e0580cf9c771e8e06bb1ba3439eef82
SuteeS27 <88422836+SuteeSaraphan@users.noreply.github.com>
2021-10-24 03:52:28 +0700

    FIX delete user by admin ver.012897
    not tested

--- admin_process/del_user_by_admin.php
+++ admin_process/del_user_by_admin.php
@@ -4,6 +4,17 @@
     $userid = $_POST['User_ID'];
 
     if ($uid == "Admin") {
+        
+        $del_submit = "SELECT * FROM submition WHERE User_ID = '$userid'";
+        $delsubm = mysqli_query($connect,$del_submit);
+                $i=0;
+                while($row = mysqli_fetch_array($delsubm)){
+                    $del_exac = "DELETE FROM exec_output WHERE Submit_ID = '$row['Submit_ID']'"
+                }
+
+        $del_course_submit="DELETE FROM submition WHERE User_ID = '$userid'";
+        mysqli_query($connect,$del_course_submit) or die(mysqli_error());
+
         $del_course = "DELETE FROM user WHERE User_ID = '$userid'";
         mysqli_query($connect,$del_course) or die(mysqli_error());
             
@@ -11,8 +22,7 @@
         mysqli_query($connect,$del_course_role) or die(mysqli_error());
 
 
-        $del_course_submit="DELETE FROM submition WHERE User_ID = '$userid'";
-        mysqli_query($connect,$del_course_submit) or die(mysqli_error());
+        
         header("location:/WebGrader/Home_admin.php");
       }
     }else{
