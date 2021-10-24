9ec58f02b739028128a83918a9cffa52af2ca2ef
SuteeS27 <88422836+SuteeSaraphan@users.noreply.github.com>
2021-10-24 14:42:32 +0700

    Update Login.php

--- Login/Login.php
+++ Login/Login.php
@@ -16,7 +16,7 @@ endif
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     
-    <title>Login Grader</title>  
+    <title>WebGrader | เข้าสู่ระบบ</title>  
 
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
