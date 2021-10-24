de20db6ea648ef0a2c69c61bc682720fb1daeb6a
SuteeS27 <88422836+SuteeSaraphan@users.noreply.github.com>
2021-10-24 12:49:02 +0700

    Update CreateCourse.php

--- Classroom/CreateCourse.php
+++ Classroom/CreateCourse.php
@@ -1,5 +1,17 @@
 <!DOCTYPE html>
-<?php $years = range(strftime("%Y", time()), strftime("%Y", date(9634894360))); 
+<?php
+    include('../config.php');
+     if(!isset($_SESSION['Username'])):
+     header("location:/WebGrader/Login/Login.php");
+    endif;
+    $userid = $_SESSION['User_ID'];
+    $checkcourserole = mysqli_query($connect,"SELECT Role FROM course_role WHERE User_ID = '$userid' ");
+    //echo $checkcourserole["Role"];
+    $result = mysqli_fetch_assoc($checkcourserole);
+    $role = $result["Role"];
+
+    $years = range(strftime("%Y", time()), strftime("%Y", date(9634894360))); 
+    
 
 
 
@@ -7,14 +19,90 @@
 <html>
 <head>
   
+  <style>
+      .container{
+        font-family: 'Kanit', sans-serif;
+
+      }
+      .cardlink{
+          color:black;
+        
+  
+      }
+      .cardlink:hover{
+          color:#292928;
+      }
+      .cardborder {
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+        border-bottom-width: 20px;
+
+      }
+
+
+      
+
+
+  </style>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
 
+     
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />   
+  
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+  
+  <!-- link font google kanit -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@500&display=swap" rel="stylesheet">
+   
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 
-  <body>
+<body class="hold-transition layout-top-nav " >
+<script>
+function confirmation(){
+    var result = confirm("Are you sure to Edit?");
+    return result;
+}
+</script>
+
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <div class="sticky-top"> <?php include "../template/navbar.php"; ?></div>
+  <!-- /.navbar -->
+
+    <div class="content">
+      <div class="container">
+       <div class="content-wrapper bg-white">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container mt-5">
+     
+<div class="row">
+
+<div class="col-md-12 col-sm-12 col-12">
+<div class="card h-100">
+  <div class="card-body border border-dark">
+    <div class="row gutters">
+      <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <h1 class="mb-2 text-dark">การเพิ่มห้องเรียน</h1>
+      </div>
+
+
 	<form method="POST" action="create_course_process.php">
 		Course:<br><input type="text" name="Course_Name">
     <br><br>
