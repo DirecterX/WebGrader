224d5e11cc749db67ac3f5a896894a72aba1536b
SuteeS27 <88422836+SuteeSaraphan@users.noreply.github.com>
2021-11-05 23:55:48 +0700

    Update register.php

--- Register/register.php
+++ Register/register.php
@@ -25,38 +25,11 @@ body{
 
                                 <form class="form-horizontal" method="post" action="#">
                                 <div class="form-group">
-                                        <label for="name" class="cols-sm-2 control-label">Student ID</label>
+                                        <label for="name" class="cols-sm-2 control-label">Username</label>
                                         <div class="cols-sm-10">
                                             <div class="input-group">
                                                 <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
                                                 <input type="text" class="form-control" name="stu_id" id="stu_id" placeholder="Student ID" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="name" class="cols-sm-2 control-label">Your Name</label>
-                                        <div class="cols-sm-10">
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-                                                <input type="text" class="form-control" name="name" id="name" placeholder="Enter your Name" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="email" class="cols-sm-2 control-label">Your Email</label>
-                                        <div class="cols-sm-10">
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-                                                <input type="text" class="form-control" name="email" id="email" placeholder="Enter your Email" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="username" class="cols-sm-2 control-label">Username</label>
-                                        <div class="cols-sm-10">
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-                                                <input type="text" class="form-control" name="username" id="username" placeholder="Enter your Username" />
                                             </div>
                                         </div>
                                     </div>
@@ -78,6 +51,34 @@ body{
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="form-group">
+                                        <label for="name" class="cols-sm-2 control-label">Firstname</label>
+                                        <div class="cols-sm-10">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                                                <input type="text" class="form-control" name="name" id="name" placeholder="Enter your Name" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                     <div class="form-group">
+                                        <label for="name" class="cols-sm-2 control-label">Surname</label>
+                                        <div class="cols-sm-10">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                                                <input type="text" class="form-control" name="name" id="name" placeholder="Enter your Name" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="email" class="cols-sm-2 control-label">E-mail</label>
+                                        <div class="cols-sm-10">
+                                            <div class="input-group">
+                                                <span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                                                <input type="text" class="form-control" name="email" id="email" placeholder="Enter your Email" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
                                     <div class="form-group ">
                                         <button type="button" class="btn btn-primary btn-lg btn-block login-button">Register</button>
                                     </div>
