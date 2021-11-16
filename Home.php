3033ac3e4aa60aab670371e27bdeeaf3a4e4d527
Palm <palm.181433@gmail.com>
2021-11-17 01:03:39 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -174,7 +174,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   $showuser = mysqli_fetch_array($sqlshowuser_q);
 
                   ?>
-                  <a href="SubmitedAssignment.php?Submit_ID=<?php echo $subid?>&Assignment_ID=<?php echo $rowq["Assignment_ID"]?>" style="color: #3D367B;"><p class="pl-3 pt-2 mr-2 border-5 rounded-1 " style="box-shadow: 0.5px 5px;background-color: #FFFFFF;"><?php echo $counq ?>.<?php echo $showuserandasss["Name"] ?> ส่งโดย <?php echo $showuser["Firstname"]; echo " ".$showuser["Surname"]  ?><label class="text-warning ml-2 float-right" style="padding-right:10px;"> <?php echo "( waiting for inspect )" ?></label></p></a>
+                  <a href="SubmitedAssignment.php?Submit_ID=<?php echo $subid?>&Assignment_ID=<?php echo $rowq["Assignment_ID"]?>" style="color: #3D367B;"><p class="pl-3 pt-2 mr-2 border-5 rounded-1 " style="box-shadow: 0.5px 5px;background-color: #FFFFFF;"><?php echo $counq ?>.<?php echo $showuserandasss["Name"] ?> ส่งโดย <?php echo $showuser["Firstname"]; echo " ".$showuser["Surname"]  ?><label class="text-warning ml-2"> <?php echo "( waiting for inspect )" ?></label></p></a>
                   <?php 
               }
             }
