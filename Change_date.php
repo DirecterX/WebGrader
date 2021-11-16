26e4b561c50c9aed0f0d9012ce4ef815d0afc7e0
WAYU SAENGPHITHAK <WavaGravity@gmail.com>
2021-11-16 21:33:08 +0700

    edit only date for EditAssignment page

---
+++ Change_date.php
@@ -0,0 +1,18 @@
+<?php
+    include('config.php');
+
+    $assignment_id = $_REQUEST['Assignment_ID'];
+    $end_date = $_REQUEST['End_date'];
+    
+    $update_date1 = $connect->prepare("UPDATE assignment SET End_date=? WHERE Assignment_ID =? AND User_ID =?");
+    $update_date1->bind_param("si", $end_date, $assignment_id);
+    $update_date1->execute();
+    if($update_date1->execute()){
+        echo "success";
+    }else{
+        echo "failed";
+    }
+
+    $update_date1->close();
+
+?>
